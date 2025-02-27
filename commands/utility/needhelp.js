8cacdc9ed14d9e4c7df8c36db6750de1243246e6
Kuba <you@example.com>
2025-02-27 20:34:11 +0100

    znova opraveno copilotem

--- commands/utility/needhelp.js
+++ commands/utility/needhelp.js
@@ -1,4 +1,3 @@
-// filepath: ./commands/needhelp.js
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
 const { botRoom } = require('../../config.json');
@@ -7,45 +6,50 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('needhelp')
         .setDescription('Upozorní na tvůj problém v kanálu hepl')
-        .addStringOption(option =>
-            option.setName('reason')
-                .setDescription('Můžeš připsat s čím potřebuješ pomoct.')
-                .setRequired(false))
-        // Nová struktura pro subcommand - musí být ve skupině kvůli Discord API
-        .addSubcommandGroup(group =>
-            group.setName('special')
-                .setDescription('Speciální typy pomoci')
-                .addSubcommand(subcommand =>
-                    subcommand
-                        .setName('modhelp')
-                        .setDescription('Pošle upozornění moderátorům.'))),
+        // Přidáváme základní subcommand
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('normal')
+                .setDescription('Standardní žádost o pomoc')
+                .addStringOption(option =>
+                    option.setName('reason')
+                        .setDescription('Můžeš připsat s čím potřebuješ pomoct.')
+                        .setRequired(false)))
+        // Přidáváme modhelp subcommand
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('modhelp')
+                .setDescription('Pošle upozornění moderátorům.')
+                .addStringOption(option =>
+                    option.setName('reason')
+                        .setDescription('Můžeš připsat s čím potřebuješ pomoct.')
+                        .setRequired(false))),
 
     async execute(interaction) {
         const { channel } = interaction;
         const userId = interaction.member.user.id;
         const username = interaction.member.user.username;
         const channelName = channel.name;
-        // Změněno z const na let, protože hodnotu měníme
         let reason = "";
+        
+        // Zjistíme, který subcommand byl použit
+        const subcommand = interaction.options.getSubcommand();
 
-        // Nový způsob kontroly subcommandu
-        if (interaction.options.getSubcommand() === 'modhelp') {
-            // Přístup k client přes interaction.client místo globální proměnné
+        if (subcommand === 'modhelp') {
             const botChannel = interaction.client.channels.cache.get(botRoom);
 
             if (interaction.options.getString("reason")) {
                 reason = interaction.options.getString("reason");
-                // Použití backticks místo uvozovek pro string interpolaci
                 botChannel.send(`Uživatel ${username} potřebuje pomoct v kanálu ${channelName} s '${reason}'.`);
             } else {
                 botChannel.send(`Uživatel ${username} potřebuje pomoct v kanálu ${channelName}`);
             }
         } else {
+            // 'normal' subcommand nebo jakýkoliv jiný
             const heplChannel = interaction.client.channels.cache.get("1344726883610263715");
             if (interaction.options.getString("reason")) {
                 reason = interaction.options.getString("reason");
-                // Použití backticks místo uvozovek pro string interpolaci
-                heplChannel.send(`Uživatel ${username} potřebuje pomoct v kanálu ${channelName} s ${reason}.`);
+                heplChannel.send(`Uživatel ${username} potřebuje pomoct v kanálu ${channelName} s '${reason}'.`);
             } else {
                 heplChannel.send(`Uživatel ${username} potřebuje pomoct v kanálu ${channelName}`);
             }
