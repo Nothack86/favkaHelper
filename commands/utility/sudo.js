6f7d5cd3be2b09e29e8774587c7f358c45217d80
Kuba <you@example.com>
2025-02-27 21:33:19 +0100

    priprava funkce sudo

--- commands/utility/sudo.js
+++ commands/utility/sudo.js
@@ -17,27 +17,18 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('sudo')
         .setDescription('Nástroje pro moderátory')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('troublemaker')
-                .setDescription('Označí potížistu')
-                .addStringOption(option =>
-                    option.setName('username')
-                        .setDescription('Jméno nového potížisty')
-                        .setRequired(true)
-                )),
+        .addStringOption(option =>
+            option.setName('parameters')
+                .setDescription('Ti co vědí ;)')
+                .setRequired(true)
+        ),
     async execute(interaction) {
         const username = interaction.member.user.username;
-
         if (interaction.member.roles.cache.has("1284041546458464328")) { // má roli moderátor
-            if (guild.members.cache.find(member => member.user.username === username)) {
-                const troublemakerUsername = interaction.options.getString(troublemakerUsername);
-                const troublemaker = guild.members.cache.find(member => member.user.username === troublemakerUsername);
-                const displayTroublemaker = getNickname(troublemaker);
-                troublemaker.roles.add('1296157529335922688'); // nastaví roli potížista
-                await interaction.reply({ content: "Člen ${displayTroublemaker} dostal roli: Potížista", ephemeral: true });
-            } else {
-                await interaction.reply({ content: "Člen nenalezen", ephemeral: true });
+            const input = interaction.options.getString(parameters);
+            const params = input.split(" ");
+            if (params[0] == "") {
+                //funkce
             }
         } else {
             await interaction.reply('${username} is not in the sudoers file, this incident will be reported');
@@ -45,3 +36,14 @@ module.exports = {
         }
     },
 };
+
+// funkce troblemaker, nechci ji smazat ale zatim na ni kaslu
+/* if (guild.members.cache.find(member => member.user.username === username)) {
+    const troublemakerUsername = interaction.options.getString(troublemakerUsername);
+    const troublemaker = guild.members.cache.find(member => member.user.username === troublemakerUsername);
+    const displayTroublemaker = getNickname(troublemaker);
+    troublemaker.roles.add('1296157529335922688'); // nastaví roli potížista
+    await interaction.reply({ content: "Člen ${displayTroublemaker} dostal roli: Potížista", ephemeral: true });
+} else {
+    await interaction.reply({ content: "Člen nenalezen", ephemeral: true });
+}*/
