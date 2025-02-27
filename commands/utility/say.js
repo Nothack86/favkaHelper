5ddc84ae098bbf27aa39e8051e2f6c946344c146
Kuba <you@example.com>
2025-02-27 21:50:43 +0100

    implementace funkce say

---
+++ commands/utility/say.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { owner } = require('../../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('say')
+        .setDescription('Nepovolený příkaz')
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('Zpráva k zobrazení')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const userId = interaction.user.id;
+        const { channel } = interaction;
+        
+        if (userId === owner) {
+            const message = interaction.options.getString('message');
+            await channel.send(message); // Přidáno await pro asynchronní volání
+            await interaction.reply({ content: 'Odesláno', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'Promiň ale tuhle funkci nemůžeš použít.', ephemeral: true });
+            console.log(`${interaction.user.username} tried to use the "say" command`);
+        }
+    },
+};
