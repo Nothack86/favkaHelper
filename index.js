48855435dbf0e8c70790f63ffd763277f18141fa
Kuba <you@example.com>
2025-02-27 20:39:20 +0100

    bot dostal zakazano dumpovat celej interaction json do console

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on(Events.InteractionCreate, async interaction => {
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	}
-	console.log(interaction);
+	//console.log(interaction);
 });
 
 // When the client is ready, run this code (only once).
