22a8fc4d855f30e605c59990706bac5ca5f9b3e1
Nothack86 <kuba@seflik.cz>
2025-02-28 12:40:49 +0100

    odepření funkce potížistům

--- commands/utility/needhelp.js
+++ commands/utility/needhelp.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 const fs = require('fs');
 const { botRoom } = require('../../config.json');
 
@@ -10,7 +10,7 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('normal')
-                .setDescription('Standardní žádost o pomoc')
+                .setDescription('Odešle žádost o pomoc do kanálu pls-help.')
                 .addStringOption(option =>
                     option.setName('reason')
                         .setDescription('Můžeš připsat s čím potřebuješ pomoct.')
@@ -37,27 +37,44 @@ module.exports = {
         
         // Zjistíme, který subcommand byl použit
         const subcommand = interaction.options.getSubcommand();
-
+        if (interaction.member.roles.cache.has("1296157529335922688")) { // pro potížisty se nespustí
+            await interaction.reply({ content: "https://tenor.com/view/chill-guy-chill-guy-just-a-chill-guy-relax-gif-1579445140409713118", ephemeral: false });
+        }
+        else {
+            
         if (subcommand === 'modhelp') {
             const botChannel = interaction.client.channels.cache.get(botRoom);
 
             if (interaction.options.getString("reason")) {
                 reason = interaction.options.getString("reason");
-                botChannel.send(`Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} s '${reason}'.`);
+                botChannel.send({
+                    content: `Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} s '${reason}'.`,
+                    flags: [MessageFlags.SuppressEmbeds]
+                });
             } else {
-                botChannel.send(`Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} .`);
+                botChannel.send({
+                    content: `Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} .`,
+                    flags: [MessageFlags.SuppressEmbeds]
+                });
             }
         } else {
             // 'normal' subcommand nebo jakýkoliv jiný
             const heplChannel = interaction.client.channels.cache.get("1344726883610263715");
             if (interaction.options.getString("reason")) {
                 reason = interaction.options.getString("reason");
-                heplChannel.send(`Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} s "${reason}".`);
+                heplChannel.send({
+                    content: `Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} s "${reason}".`,
+                    flags: [MessageFlags.SuppressEmbeds]
+                });
             } else {
-                heplChannel.send(`Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} .`);
+                heplChannel.send({
+                    content: `Uživatel <@${userId}> potřebuje pomoct v kanálu ${messageLink} .`,
+                    flags: [MessageFlags.SuppressEmbeds]
+                });
             }
         }
         
         await interaction.reply({ content: "Upozornění odesláno.", ephemeral: false });
+        }
     },
 };
